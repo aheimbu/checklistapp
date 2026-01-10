1f3321936ad01820d2fc9ede8c3dd7df898a878a
aheim <a.heimbuch.r@gmx.de>
2026-01-10 21:25:14 +0100

    project structure, mvc concept

---
+++ src/main/java/aheim/checklistapp/App.java
@@ -0,0 +1,13 @@
+package aheim.checklistapp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! It Works!" );
+    }
+}
